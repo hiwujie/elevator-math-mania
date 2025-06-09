32ad90691671362b0d77e163da238fabaee7ffb6
Jie Wu <hiwujie@gmail.com>
2025-06-09 05:45:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <LocaleProvider> {/* Wrap children with LocaleProvider */}
           {children}
         </LocaleProvider>
